cf02936745f5ebac1cfbcccec2215b445dfd36ff
Edward <edwardnz2017@gmail.com>
2020-10-19 23:32:57 +0900

    Updates

---
+++ precache-manifest.2426b5daee367fdf3eeb4c0dc13b1be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b39f83a9b212c590f5462138e6fdbca",
+    "url": "/cv-generator/index.html"
+  },
+  {
+    "revision": "a976c483d1ca24d28b7d",
+    "url": "/cv-generator/static/css/main.746770fe.chunk.css"
+  },
+  {
+    "revision": "82e6d1d718eec5adf657",
+    "url": "/cv-generator/static/js/2.4cb7dea6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-generator/static/js/2.4cb7dea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a976c483d1ca24d28b7d",
+    "url": "/cv-generator/static/js/main.d44a0c0c.chunk.js"
+  },
+  {
+    "revision": "7b9c8d8c6d8ad8f4953d",
+    "url": "/cv-generator/static/js/runtime-main.5fc965f0.js"
+  }
+]);
